c665f9eef6fe6acc3b786402998f1259790463ca
leonard <40259905+leonard246@users.noreply.github.com>
2019-03-03 21:52:34 +1100

    working copy no design

--- src/index.js
+++ src/index.js
@@ -5,24 +5,30 @@ import ReactDOM from 'react-dom'
 class CodeWindow extends React.Component {
     constructor(props) { 
         super(props);
-        this.state = {
-            code: "",
-            lang: this.props.lang,
-        };
 
         this.handleChange = this.handleChange.bind(this);
+        this.getPlaceholder = this.getPlaceholder.bind(this);
     }
 
     handleChange(e) {
-        this.setState({ code: e.target.value });
-        this.props.renderCode(this.state.code, this.state.lang);
+        this.props.renderCode(e.target.value, this.props.lang);
+    }
+
+    getPlaceholder = () => {
+        if(this.props.lang === "")
+            return "HTML";
+        else
+            return this.props.lang;
     }
 
     render() {
-        return (
-        <textarea placeholder={`Enter ${this.state.lang} here`} onChange={this.handleChange} style={{width: '30%', height: '100%'}}>
-        </textarea>
-    )}
+        return ( 
+            <textarea
+                    placeholder={`Enter ${this.getPlaceholder()} code here`}
+                    onChange={this.handleChange}
+                    style={{width: `30vw`, height: '15vw'}} />
+        );
+    }
 }
 
 // Parent
@@ -31,38 +37,35 @@ class Result extends React.Component
     constructor(props) {
         super(props);
         this.state = {
-            result: 'Result',
-            lang: "-",
+            HTMLCode: 'HTML', 
+            CSSCode: 'CSS',
+            JSCode: 'JS',
         };
+
         this.renderResult = this.renderResult.bind(this);
     }
     
     // For passing to child
-    renderResult(code, language) {
-        this.setState({result: code, lang: language});
+    renderResult(input,lang) {
+        if(lang === "HTML")
+            this.setState({HTMLCode: input});
+
+        if(lang === "CSS")
+            this.setState({CSSCode: input});
+
+        if(lang === "JS")
+            this.setState({JSCode: input});
     }
     
     generateContent = () => {
-        // content[0] = HTML, content[1] = CSS, content[2] = JS
-        let content = Array(3).fill(""); 
-
-        if(this.state.lang === "HTML")
-            content[0] = this.state.result;
-        
-        if(this.state.lang === "CSS")
-            content[1] = this.state.result;
-
-        if(this.state.lang === "JS")
-            content[2] = this.state.result;
-
         return `<!DOCTYPE html>
                 <html>
                 <head>
-                    <style>${content[1]}</style>
-                    <script>${content[2]}</script>
+                    <style>${this.state.CSSCode}</style>
+                    <script>${this.state.JSCode}</script>
                 </head>
                 <body>
-                    ${content[0]}
+                    ${this.state.HTMLCode}
                 </body>
                 </html>`
     }
@@ -71,13 +74,11 @@ class Result extends React.Component
         return (
         <div>
             <h1>Enter Code Below</h1>
-            <div style={{ height: '15vw'}}>
-                <CodeWindow renderCode={this.renderResult.bind(this)} lang={"HTML"}/>
-                <CodeWindow renderCode={this.renderResult.bind(this)} lang={"CSS"}/>
-                <CodeWindow renderCode={this.renderResult.bind(this)} lang={"JS"}/>
-            </div>
-            <div style={{height: '25vw'}}>
-                <iframe style={{width: '90.5%', height: '100%'}} srcDoc={this.generateContent()}></iframe>
+            <div>
+                <CodeWindow code={this.state.HTMLCode} lang={"HTML"} renderCode={this.renderResult}/>
+                <CodeWindow code={this.state.CSSCode} lang={"CSS"} renderCode={this.renderResult}/>
+                <CodeWindow code={this.state.JSCode} lang={"JS"} renderCode={this.renderResult}/>
+                <iframe style={{width: '90vw', height: '50vh'}} srcDoc={this.generateContent()}></iframe>
             </div>
         </div>
         )
